2ecae8b8e22d252093c180f352f575e53cbd1b6a
tobi-creates <sinonelesinakotobi@gmail.com>
2026-06-11 11:04:40 +0200

    Add complete

--- hello.py
+++ hello.py
@@ -2,3 +2,5 @@ import pandas as pd
 import numpy as np
 print("Hello, World!")
 print("This is a simple Python script that prints 'Hello, World!' and imports pandas and numpy libraries.")
+print("Pandas version:", pd.__version__)
+print("Numpy version:", np.__version__)
